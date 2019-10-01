9c170f1c454c5c6d1f3ee36f1d0076d208b93328
José Artur Kassala <52246841+arturjose0@users.noreply.github.com>
2019-10-01 14:22:54 +0100

    nota
    mensionar-me caso usares

---
+++ pt-ao.h
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+#define escreval( cout<<
+#define principal main
+#define letra string
+#define caracter char
+#define logica bool
+#define leia( cin>>  
+#define retorno return
+#define faca do
+#define enquanto while
+#define para for
+#define p endl
+#define travar break
+#define <- =
+#define caso case
+#define escolha switch
